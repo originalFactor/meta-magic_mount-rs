968deaa940717422b81c80db60905537146153d2
originalFactor <2438926613@qq.com>
2026-03-28 17:49:07 +0800

    add new webui

---
+++ webui/_next/static/chunks/10g-88bhsqh19.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,59718,t=>{t.v(o=>Promise.all(["static/chunks/09fi5w8_m_f8g.js"].map(o=>t.l(o))).then(()=>o(34222)))}]);
